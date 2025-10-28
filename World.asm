c8e25e502054cd5c4a91c76ff046ba14b3d442e9
Olutoye Odufowokan <Dtv48@proton.me>
2025-10-28 16:55:24 -0500

    Update World.asm

--- World.asm
+++ World.asm
@@ -1,6 +1,6 @@
 # Olutoye Odufowokan
 # CS 2340.009
-# In-line Project Description: A simple binary game that tests the user's knowledge of binary numbers. Recreated using MIPS assembly language. 
+# In-line Project Description: A simple binary game that tests the user's knowledge of binary numbers. Recreated using MIPS assembly language.
 .data
 startingPromptOne: .asciiz "Welcome to the Binary Game!\n"
 startingPromptTwo: .asciiz "Please read the project documentation if you don't know the rules for how the game operates\n"
@@ -9,11 +9,31 @@ startingPromptThree: .asciiz "Let's begin!\n"
 player: .asciiz "Player" # Placeholder text as the player would need to be mentioned multiple times
 computer: .asciiz "Computer" # Placeholder text as the computer would need to be mentioned multiple times
 
+current_level: .word 1
+line: .asciiz "----------------------------------------\n"
+lines_displayed: .word 0
+game_over: word 0
 
+.text
 .main
+    # Printing my three starting prompts
+    li $v0, 4
+    la $a0, startingPromptOne
+    syscall
+    li $v0, 4
+    la $a0, startingPromptTwo
+    syscall
+    li $v0, 4
+    la $a0, startingPromptThree
+    syscall
 
-# World.asm controls the game's logic and flow. It be functioning as game loop that call upon the assembly modules for it to work
-# I'll implement a timeout personally for just proper coding practice even though it not technically require
-li  $t0, 0 # loop counter = 0 
-li 
-loop:
+    li $t0, 1
+    sw $t0, current_level
+
+    li $t1, 0
+    sw $t1, lines_displayed
+
+    # World.asm controls the game's logic and flow. It be functioning as game loop that call upon the assembly modules for it to work
+    # I'll implement a timeout personally for just proper coding practice even though it not technically require
+
+    game_loop:
